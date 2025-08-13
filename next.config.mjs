4d7b25b5ab05ee1a6a1f7321caf1508c79fedd8e
ShardulVanage <vsharadul@gmail.com>
2025-08-13 14:38:53 +0530

    error fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
     domains: ['res.cloudinary.com','images.unsplash.com','plus.unsplash.com','zep-research.pockethost.io',''],
   },
   experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ['pdf-parse']
+    serverActions: {},
   },
+  serverComponentsExternalPackages: ['pdf-parse']
 };
 
 export default nextConfig;
