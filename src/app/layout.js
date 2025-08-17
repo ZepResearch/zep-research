bd95688a7c31e9f3ee580fceff54d29a9543354e
ShardulVanage <vsharadul@gmail.com>
2025-08-17 14:24:36 +0530

    umami code  added

--- src/app/layout.js
+++ src/app/layout.js
@@ -150,6 +150,13 @@ const demoData = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        <script
+          async
+          src="https://analytics.zepresearch.com/script.js"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
